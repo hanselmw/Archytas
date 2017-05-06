77ea6c418544d8529a5859bcdbc93be8bd9daaeb
Hansel Wave <arastafiel@gmail.com>
2017-05-06 13:39:26 -0600

    Add Temperature functions

--- src/cljx/archytas/sensors/temperature.clj
+++ src/cljx/archytas/sensors/temperature.clj
@@ -1,2 +1,47 @@
 (ns archytas.sensors.temperature)
 
+(defmulti get-temperature-from-voltage
+  "Get temperature from voltage in the specified unit of measurement symbol for temperature."
+  (fn [unit _] unit )
+  )
+
+(defmethod get-temperature-from-voltage :celsius
+  [_ voltage]
+  (float (* (- voltage 0.5) 100)))
+
+(defmethod get-temperature-from-voltage :fahrenheit
+  [_ voltage]
+  (float (+ (* (/ 9 5) (* (- voltage 0.5) 100)) 32)))
+
+(defmethod get-temperature-from-voltage :kelvin
+  [_ voltage]
+  (float (+ (* (- voltage 0.5) 100) 273.15)))
+
+
+(defmulti convert-temperature
+  "Convert temperature from unit, to unit  according to temp value."
+  (fn [from to _] [from to]))
+
+(defmethod convert-temperature [:celsius :fahrenheit]
+  [_ _ temp]
+  (float (+ (* temp (/ 9 5)) 32)))
+
+(defmethod convert-temperature [:celsius :kelvin]
+  [_ _ temp]
+  (float (+ temp 273.15)))
+
+(defmethod convert-temperature [:fahrenheit :celsius]
+  [_ _ temp]
+  (float (* (- temp 32) (/ 5 9))))
+
+(defmethod convert-temperature [:fahrenheit :kelvin]
+  [_ _ temp]
+  (float (* (+ temp 459.67) (/ 5 9))))
+
+(defmethod convert-temperature [:kelvin :celsius]
+  [_ _ temp]
+  (float (- temp 273.15)))
+
+(defmethod convert-temperature [:kelvin :fahrenheit]
+  [_ _ temp]
+  (float (- (* temp (/ 9 5)) 459.67)))
